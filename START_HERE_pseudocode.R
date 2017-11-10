8e5955b54f1c6a6b08593c92a6ffd9b55e9d68f1
Kate Vendrely <kvendrel@nd.edu>
2017-11-10 10:34:30 -0500

    initial

--- START_HERE_pseudocode.R
+++ START_HERE_pseudocode.R
@@ -1,9 +1,10 @@
-# Problem 1:
+############QUESTION 1 ######################
 #   
-#   align each of the reference files with muscle
+# align each of the reference files with muscle
+./muscle -in 
 # make HMM profiles from each alignment with hmmbuild
-# you could do the above in a loop over *.ref, but I don't think it is necessary...
-# 
+./hmm
+
 # loop over all *.fasta
 # run hmmsearch with the --tblout option and use the index variable to specify a unique output file
 # do the hmmsearch step 3 times, one per HMM profile
@@ -14,21 +15,44 @@
 # trim the 1st column contents to just the last bit that is a "species" code
 # 
 # 
-# Problem 2:
-# open the fasta file for reading line by line in Python
-# load teh fasta file into a vector using scan() in R
-# 
+#############QUESTION 2 ################
+# load the fasta file into a vector using scan() in R
+fasta=scan(file="Exercise11_files/Problem2/motifsort.fasta", what=character())
+
 # create three output files to be written to (motif1, motif2, other)
-# 
+ writeLines()
+
 # loop through the input file line by line
+for(i in 1:length(fasta)) {
 # if it has a ">" save that line for later
-# else it is a sequence file and there is more to do
-#     if it matches motif1
-#         write the saved >sequenceN
-#         write the sequence line
-#     else if it matches motif2
-#          write the saved >sequenceN
-#          write the sequence line
-#     else
-#          write the saved >sequenceN
-#         write the sequence line
+  if(grepl(">",fasta[i])){
+    
+    # else it is a sequence file and there is more to do
+    #     if it matches motif1
+    #         write the saved >sequenceN
+    #         write the sequence line
+  }else{
+    if(grepl("motif1",fasta[i])){
+      
+    }
+    seqLength=nchar(fasta[i])
+    
+    numG=nchar(fasta[i])-nchar(gsub("G","",fasta[i]))
+    numC=nchar(fasta[i])-nchar(gsub("C","",fasta[i]))
+    if(seqLength<=14){
+      Tm=2*(numG+numC)+2*seqLength
+      #     else if it matches motif2
+      #          write the saved >sequenceN
+      #          write the sequence line
+     
+    }else{
+      
+      
+    }else{
+      #     else
+      #          write the saved >sequenceN
+      #         write the sequence line
+    }
+    
+    }
+
