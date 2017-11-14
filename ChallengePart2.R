2035b7c8aa32f4b865be439c10ed27145a5f0b9a
Patrick LeBlanc <patrick.m.leblanc@gmail.com>
2017-11-14 16:12:12 -0500

    Challenge Part 2 Completed

--- ChallengePart2.R
+++ ChallengePart2.R
@@ -2,6 +2,7 @@ rm(list=ls())
 
 #load string package
 library(stringr)
+library(seqinr)
 
 #Scan in files
 fasta=scan(file="motifsort.fasta",what=character(),sep="\n")
@@ -10,33 +11,48 @@ fasta=scan(file="motifsort.fasta",what=character(),sep="\n")
 motifOne='AKKPRVZE'
 motifTwo='AAQWWRNYGG'
 
-motif1=as.data.frame(matrix(ncol = 1, nrow = 10), sep = "\n")
-motif2=as.data.frame(matrix(ncol = 1, nrow = 10), sep = "\n")
-NoMotif=as.data.frame(matrix(ncol = 1, nrow = 10), sep = "\n")
+motif1Names=vector(mode='list',length=11)
+motif2Names=vector(mode='list',length=13)
+NoMotifNames=vector(mode='list',length=66)
 
-a=2
-b=2
-c=2
+motif1=vector(mode='list',length=11)
+motif2=vector(mode='list',length=13)
+NoMotif=vector(mode='list',length=66)
+
+a=1
+b=1
+c=1
 
 for (i in 1:length(fasta)){
   if (str_detect(fasta[i],motifOne)==TRUE){
-    motif1[a-1,1]=fasta[i-1]
-    motif1[a,1]=fasta[i]
-    a=a+2
+    motif1Names[a]=fasta[i-1]
+    motif1[a]=fasta[i]
+    a=a+1
     }
   else if (str_detect(fasta[i],motifTwo)==TRUE){
-    motif2[b-1,1]=fasta[i-1]
-    motif2[b,1]=fasta[i]
-    b=b+2
+    motif2Names[b]=fasta[i-1]
+    motif2[b]=fasta[i]
+    b=b+1
   }
   else if (str_detect(fasta[i],'sequence')==FALSE){
-    NoMotif[c-1,1]=fasta[i-1]
-    NoMotif[c,1]=fasta[i]
-    c=c+2
+    NoMotifNames[c]=fasta[i-1]
+    NoMotif[c]=fasta[i]
+    c=c+1
   }
 }
 
-write.table(motif1, file='motif1.fasta', sep = "\n")
-write.table(motif2, file='motif2.fasta', sep = "\n")
-write.table(NoMotif, file='NoMotif.fasta', sep = "\n")
+for (i in 1:length(motif1Names)){
+  motif1Names[i]=str_replace_all(motif1Names[i],">","")
+}
 
+for (i in 1:length(motif2Names)){
+  motif2Names[i]=str_replace_all(motif2Names[i],">","")
+}
+
+for (i in 1:length(NoMotifNames)){
+  NoMotifNames[i]=str_replace_all(NoMotifNames[i],">","")
+}
+
+write.fasta(sequences=motif1, names=motif1Names,file.out="motif1.fasta")
+write.fasta(sequences=motif2, names=motif2Names,file.out="motif2.fasta")
+write.fasta(sequences=NoMotif, names=NoMotifNames,file.out="NoMotif.fasta")
